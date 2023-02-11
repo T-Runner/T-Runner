493c2c760b8d7be380357019d473a45f7f91c97e
Vieup <tranthitruongvydhqn@gmail.com>
2023-02-11 13:48:09 +0700

    update code

--- ReactJs/src/constants.js
+++ ReactJs/src/constants.js
@@ -1,43 +1,123 @@
 export const defaultMember = {
-  fullname: '',
-  username: '',
-  role: '',
-  groupName: '',
-  email: '',
-  phoneNumber: '',
-  DOB: '',
-  weight: '',
-  gender: '',
+  fullname: "",
+  username: "",
+  role: "",
+  groupName: "",
+  email: "",
+  phoneNumber: "",
+  DOB: "",
+  weight: "",
+  gender: "",
 };
 
 export const defaultExerciseLibrary = {
-  name: '',
-  img: ''
-}
+  name: "",
+  img: "",
+};
 
 const createRoleData = (role) => {
   return { role };
 };
 
 export const roleDatas = [
-  createRoleData('Super Admin'),
-  createRoleData('Admin'),
-  createRoleData('Coach'),
-  createRoleData('Member'),
-  createRoleData('Customer Service'),
+  createRoleData("Super Admin"),
+  createRoleData("Admin"),
+  createRoleData("Coach"),
+  createRoleData("Member"),
+  createRoleData("Customer Service"),
 ];
 
-const createData = (fullname, username, role, groupName, email, phoneNumber, DOB, weight, gender) => {
-  return { fullname, username, role, groupName, email, phoneNumber, DOB, weight, gender };
+const createData = (
+  fullname,
+  username,
+  role,
+  groupName,
+  email,
+  phoneNumber,
+  DOB,
+  weight,
+  gender
+) => {
+  return {
+    fullname,
+    username,
+    role,
+    groupName,
+    email,
+    phoneNumber,
+    DOB,
+    weight,
+    gender,
+  };
 };
 
 export const memberdatas = [
-  createData('Tino Phan', 'tinophan247', 'Super Admin', 'TMA Group', 'tinophan@gmail.com', '0999888777', '1/1/2000', '75kg', 'Male'),
-  createData('Han Trinh', 'peDau', 'Admin', 'OTF Group', 'hanytrinh@gmail.com', '0987654321', '2/2/1995', '45kg', 'Female'),
-  createData('Anh Phan', 'anhphan123', 'Coach', 'TMA Group', 'anhphan@gmail.com', '0999777666', '3/3/2000', '80kg', 'Male'),
-  createData('Duc Vo', 'ducvo789', 'Member', 'CT4 Group', 'ducvo@gmail.com', '0999555444', '4/4/2000', '65kg', 'Male'),
-  createData('Huy Nguyen', 'anhHatDe', 'Member', 'PDA Group', 'huynguyen@gmail.com', '0999444333', '5/5/2000', '75kg', 'Male'),
-  createData('Tony Tong', 'tonytong567', 'Customer Service', 'TMA Group', 'tonytong@gmail.com', '0999111222', '6/5/1987', '70kg', 'Male'),
+  createData(
+    "Tino Phan",
+    "tinophan247",
+    "Super Admin",
+    "TMA Group",
+    "tinophan@gmail.com",
+    "0999888777",
+    "1/1/2000",
+    "75kg",
+    "Male"
+  ),
+  createData(
+    "Han Trinh",
+    "peDau",
+    "Admin",
+    "OTF Group",
+    "hanytrinh@gmail.com",
+    "0987654321",
+    "2/2/1995",
+    "45kg",
+    "Female"
+  ),
+  createData(
+    "Anh Phan",
+    "anhphan123",
+    "Coach",
+    "TMA Group",
+    "anhphan@gmail.com",
+    "0999777666",
+    "3/3/2000",
+    "80kg",
+    "Male"
+  ),
+  createData(
+    "Duc Vo",
+    "ducvo789",
+    "Member",
+    "CT4 Group",
+    "ducvo@gmail.com",
+    "0999555444",
+    "4/4/2000",
+    "65kg",
+    "Male"
+  ),
+  createData(
+    "Huy Nguyen",
+    "anhHatDe",
+    "Member",
+    "PDA Group",
+    "huynguyen@gmail.com",
+    "0999444333",
+    "5/5/2000",
+    "75kg",
+    "Male"
+  ),
+  createData(
+    "Tony Tong",
+    "tonytong567",
+    "Customer Service",
+    "TMA Group",
+    "tonytong@gmail.com",
+    "0999111222",
+    "6/5/1987",
+    "70kg",
+    "Male"
+  ),
 ];
 
 const createDataExercise = (img, name) => {
@@ -45,37 +125,171 @@ const createDataExercise = (img, name) => {
 };
 
 export const exercisesdata = [
-  createDataExercise('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQe8BtgN04salqSqXG_hA4lb-WDwetpZprN0jtb_ucY95CpIUXWyTTvVMCEBMEhRgKvg6k&usqp=CAU', 'Jogging with Treadmill'),
-  createDataExercise('https://blog.o2fitnessclubs.com/hubfs/les%20mills%202%20.jpg', 'Squad with Dumbbell'),
-  createDataExercise('https://www.jtxfitness.com/media/catalog/product/s/t/studio_bike_from_jtx_fitness_-_built_for_durability_stability_performance_1_1.jpg', 'Cardio with Bike'),
-  createDataExercise('https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/determined-men-exercising-on-rowing-machines-at-royalty-free-image-1625135789.jpg?crop=1.00xw:0.753xh;0,0.169xh&resize=1200:*', 'Rower Exercises'),
-  createDataExercise('https://hips.hearstapps.com/hmg-prod/images/sportswoman-doing-sit-ups-with-medicine-ball-on-royalty-free-image-1040221548-1548258778.jpg', 'Medicine Ball Exercises'),
-  createDataExercise('https://health.clevelandclinic.org/wp-content/uploads/sites/3/2021/05/bicycleAbCruches-1216161742_770x553-650x428.jpg', 'Ab workout'),
-  createDataExercise('https://assets.gqindia.com/photos/5cee7eb00379a73d25177759/master/pass/Pushup.jpg', 'Push up'),
-  createDataExercise('https://www.jtxfitness.com/media/catalog/product/cache/aea7ede9c4f8eec24074fda02ef96ae3/h/o/home_cross_trainer_1.jpg', 'Strider workout'),
-  createDataExercise('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRxAnpWr7QutxIWg_B4ekGMn1YMVqYTLffguA&usqp=CAU', 'Bosu workout'),
-  createDataExercise('https://www.stack.com/wp-content/uploads/2013/03/Decline-Bench1.png', 'Decline Bench workout'),
-  createDataExercise('https://cdn.shopify.com/s/files/1/1283/2557/articles/Resistance_Band_Exercises_1024x.jpg?v=1645052429', 'Resistance bands workout'),
-  createDataExercise('https://www.mensjournal.com/wp-content/uploads/2018/05/1380-dumbbell-curl1.jpg?quality=86&strip=all', 'Dumbbell workout'),
+  createDataExercise(
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQe8BtgN04salqSqXG_hA4lb-WDwetpZprN0jtb_ucY95CpIUXWyTTvVMCEBMEhRgKvg6k&usqp=CAU",
+    "Jogging with Treadmill"
+  ),
+  createDataExercise(
+    "https://blog.o2fitnessclubs.com/hubfs/les%20mills%202%20.jpg",
+    "Squad with Dumbbell"
+  ),
+  createDataExercise(
+    "https://www.jtxfitness.com/media/catalog/product/s/t/studio_bike_from_jtx_fitness_-_built_for_durability_stability_performance_1_1.jpg",
+    "Cardio with Bike"
+  ),
+  createDataExercise(
+    "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/determined-men-exercising-on-rowing-machines-at-royalty-free-image-1625135789.jpg?crop=1.00xw:0.753xh;0,0.169xh&resize=1200:*",
+    "Rower Exercises"
+  ),
+  createDataExercise(
+    "https://hips.hearstapps.com/hmg-prod/images/sportswoman-doing-sit-ups-with-medicine-ball-on-royalty-free-image-1040221548-1548258778.jpg",
+    "Medicine Ball Exercises"
+  ),
+  createDataExercise(
+    "https://health.clevelandclinic.org/wp-content/uploads/sites/3/2021/05/bicycleAbCruches-1216161742_770x553-650x428.jpg",
+    "Ab workout"
+  ),
+  createDataExercise(
+    "https://assets.gqindia.com/photos/5cee7eb00379a73d25177759/master/pass/Pushup.jpg",
+    "Push up"
+  ),
+  createDataExercise(
+    "https://www.jtxfitness.com/media/catalog/product/cache/aea7ede9c4f8eec24074fda02ef96ae3/h/o/home_cross_trainer_1.jpg",
+    "Strider workout"
+  ),
+  createDataExercise(
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRxAnpWr7QutxIWg_B4ekGMn1YMVqYTLffguA&usqp=CAU",
+    "Bosu workout"
+  ),
+  createDataExercise(
+    "https://www.stack.com/wp-content/uploads/2013/03/Decline-Bench1.png",
+    "Decline Bench workout"
+  ),
+  createDataExercise(
+    "https://cdn.shopify.com/s/files/1/1283/2557/articles/Resistance_Band_Exercises_1024x.jpg?v=1645052429",
+    "Resistance bands workout"
+  ),
+  createDataExercise(
+    "https://www.mensjournal.com/wp-content/uploads/2018/05/1380-dumbbell-curl1.jpg?quality=86&strip=all",
+    "Dumbbell workout"
+  ),
 ];
 
 //fake data group list
-const createDataGroups = (groupName, desc, location, sport, groupType, createdDate, totalRunners, active) => {
-  return { groupName, desc, location, sport, groupType, createdDate, totalRunners, active };
+const createDataGroups = (
+  groupName,
+  desc,
+  location,
+  sport,
+  groupType,
+  createdDate,
+  totalRunners,
+  active
+) => {
+  return {
+    groupName,
+    desc,
+    location,
+    sport,
+    groupType,
+    createdDate,
+    totalRunners,
+    active,
+  };
 };
 
 export const groupData = [
-  createDataGroups('Group A', 'We are the one. We ride, we run...', 'Tucson, Oklahoma', 'Canoe', 'Company/Workplace', '01/15/2023', 200, 'Disabled'),
-  createDataGroups('Group B', 'Enjoy the moment that you...', 'Sioux Falls, Wisconsin', 'CrossFit', 'Other ', '01/07/2023', 178, 'Disabled'),
-  createDataGroups('Group C', 'No description', 'Baytown, Oregon', 'E-Bike Ride', 'Racing Team', '12/21/2022', 23, 'Disabled'),
-  createDataGroups('Group C', 'No description', 'Baytown, Oregon', 'E-Bike Ride', 'Racing Team', '12/21/2022', 23, 'Disabled'),
-  createDataGroups('Group C', 'No description', 'Baytown, Oregon', 'E-Bike Ride', 'Racing Team', '12/21/2022', 23, 'Disabled'),
-  createDataGroups('Group C', 'No description', 'Baytown, Oregon', 'E-Bike Ride', 'Racing Team', '12/21/2022', 23, 'Disabled'),
-  createDataGroups('Group C', 'No description', 'Baytown, Oregon', 'E-Bike Ride', 'Racing Team', '12/21/2022', 23, 'Disabled'),
-  createDataGroups('Group C', 'No description', 'Baytown, Oregon', 'E-Bike Ride', 'Racing Team', '12/21/2022', 23, 'Disabled'),
-  createDataGroups('Group C', 'No description', 'Baytown, Oregon', 'E-Bike Ride', 'Racing Team', '12/21/2022', 23, 'Disabled'),
+  createDataGroups(
+    "Group A",
+    "We are the one. We ride, we run...",
+    "Tucson, Oklahoma",
+    "Canoe",
+    "Company/Workplace",
+    "01/15/2023",
+    200,
+    "Disabled"
+  ),
+  createDataGroups(
+    "Group B",
+    "Enjoy the moment that you...",
+    "Sioux Falls, Wisconsin",
+    "CrossFit",
+    "Other ",
+    "01/07/2023",
+    178,
+    "Disabled"
+  ),
+  createDataGroups(
+    "Group C",
+    "No description",
+    "Baytown, Oregon",
+    "E-Bike Ride",
+    "Racing Team",
+    "12/21/2022",
+    23,
+    "Disabled"
+  ),
+  createDataGroups(
+    "Group C",
+    "No description",
+    "Baytown, Oregon",
+    "E-Bike Ride",
+    "Racing Team",
+    "12/21/2022",
+    23,
+    "Disabled"
+  ),
+  createDataGroups(
+    "Group C",
+    "No description",
+    "Baytown, Oregon",
+    "E-Bike Ride",
+    "Racing Team",
+    "12/21/2022",
+    23,
+    "Disabled"
+  ),
+  createDataGroups(
+    "Group C",
+    "No description",
+    "Baytown, Oregon",
+    "E-Bike Ride",
+    "Racing Team",
+    "12/21/2022",
+    23,
+    "Disabled"
+  ),
+  createDataGroups(
+    "Group C",
+    "No description",
+    "Baytown, Oregon",
+    "E-Bike Ride",
+    "Racing Team",
+    "12/21/2022",
+    23,
+    "Disabled"
+  ),
+  createDataGroups(
+    "Group C",
+    "No description",
+    "Baytown, Oregon",
+    "E-Bike Ride",
+    "Racing Team",
+    "12/21/2022",
+    23,
+    "Disabled"
+  ),
+  createDataGroups(
+    "Group C",
+    "No description",
+    "Baytown, Oregon",
+    "E-Bike Ride",
+    "Racing Team",
+    "12/21/2022",
+    23,
+    "Disabled"
+  ),
 ];
-
 
 //fake data location list
 const createLocationData = (value) => {
@@ -83,11 +297,11 @@ const createLocationData = (value) => {
 };
 
 export const locationDatas = [
-  createLocationData('Hà Nội'),
-  createLocationData('Hà Nam'),
-  createLocationData('Hà Tĩnh'),
-  createLocationData('Hải Dương'),
-  createLocationData('Hải Phòng'),
+  createLocationData("Hà Nội"),
+  createLocationData("Hà Nam"),
+  createLocationData("Hà Tĩnh"),
+  createLocationData("Hải Dương"),
+  createLocationData("Hải Phòng"),
 ];
 
 //fake data group type
@@ -96,11 +310,11 @@ const createGroupTypeData = (value) => {
 };
 
 export const groupTypeDatas = [
-  createGroupTypeData('Racing Team'),
-  createGroupTypeData('Company/Workplace'),
-  createGroupTypeData('Club'),
-  createGroupTypeData('Shop'),
-  createGroupTypeData('Other'),
+  createGroupTypeData("Racing Team"),
+  createGroupTypeData("Company/Workplace"),
+  createGroupTypeData("Club"),
+  createGroupTypeData("Shop"),
+  createGroupTypeData("Other"),
 ];
 
 //fake data active
@@ -109,6 +323,124 @@ const createActiveData = (value) => {
 };
 
 export const activeDatas = [
-  createActiveData('Enable'),
-  createActiveData('Disable'),
+  createActiveData("Enable"),
+  createActiveData("Disable"),
+];
+
+//fake data group list
+const createDataSports = (
+  sportName,
+  sportType,
+  lastModifiedBy,
+  lastModifiedDate,
+  active
+) => {
+  return { sportName, sportType, lastModifiedBy, lastModifiedDate, active };
+};
+
+export const sportData = [
+  createDataSports(
+    "Canoe",
+    "Water Sports",
+    "Gloria Yort",
+    "01/15/2023",
+    "Disabled"
+  ),
+  createDataSports(
+    "CrossFit",
+    "Other Sports",
+    "Miranda Streich",
+    "01/07/2023",
+    "Disabled"
+  ),
+  createDataSports(
+    "E-Bike Ride",
+    "Cycle Sports",
+    "Kyle Baile",
+    "01/12/2023",
+    "Disabled"
+  ),
+  createDataSports(
+    "Golf",
+    "Other Sports",
+    "Neil Mayert",
+    "12/21/2022",
+    "Disabled"
+  ),
+  createDataSports(
+    "Run",
+    "Foot Sports",
+    "Deanna Daniel",
+    "12/04/2022",
+    "Enable"
+  ),
+  createDataSports(
+    "Ride",
+    "Cycle Sports",
+    "Gloria Yort",
+    "01/01/2023",
+    "Enable"
+  ),
+  createDataSports(
+    "Row",
+    "Water Sports",
+    "Gloria Yort",
+    "12/19/2022",
+    "Enable"
+  ),
+  createDataSports(
+    "Sail",
+    "Water Sports",
+    "Deanna Daniel",
+    "12/28/2022",
+    "Disabled"
+  ),
+  createDataSports(
+    "Skateboard",
+    "Other Sports",
+    "Deanna Daniel",
+    "12/28/2022",
+    "Disabled"
+  ),
+  createDataSports(
+    "Surf",
+    "Water Sports",
+    "Gloria Yort",
+    "12/28/2022",
+    "Disabled"
+  ),
+  createDataSports(
+    "Swim",
+    "Water Sports",
+    "Gloria Yort",
+    "12/28/2022",
+    "Disabled"
+  ),
+  createDataSports(
+    "Walk",
+    "Foot Sports",
+    "Debra Klein",
+    "12/28/2022",
+    "Enable"
+  ),
+  createDataSports(
+    "Yoga",
+    "Other Sports",
+    "Gloria Yort",
+    "12/28/2022",
+    "Disabled"
+  ),
+];
+
+//fake data active
+const createSportTypeData = (value) => {
+  return { value };
+};
+
+export const sportTypeDatas = [
+  createSportTypeData("Foot Sports"),
+  createSportTypeData("Cycle Sports"),
+  createSportTypeData("Water Sports"),
+  createSportTypeData("Winter Sports"),
+  createSportTypeData("Other Sports"),
 ];
--- ReactJs/src/views/pages/AdminSide/SportManagement/index.jsx
+++ ReactJs/src/views/pages/AdminSide/SportManagement/index.jsx
@@ -1,14 +1,57 @@
-import React from 'react'
-import PageLayout from '../../../components/PageLayout';
+import React, { useEffect, useState } from "react";
+import PageLayout from "../../../components/PageLayout";
+import CreateEditSport from "./create-edit-sport";
+import ListSport from "./list-sport";
 
 const SportManagement = () => {
+  const [isCreate, setIsCreated] = useState(false);
+  const [isOpenEdit, setIsOpenEdit] = useState(false);
+  const handleClose = () => {
+    setIsCreated(false);
+  };
+
+  const handleCreate = () => {
+    setIsCreated(true);
+  };
+
+  const handleOpenEdit = () => {
+    setIsOpenEdit(true);
+  };
+
+  const handleCloseEdit = () => {
+    setIsOpenEdit(false);
+  };
+
+  useEffect(() => {
+    console.log(isOpenEdit);
+    console.log(isOpenEdit);
+  }, [isOpenEdit]);
+
   return (
     <div>
       <PageLayout>
-        Sports
+        {isCreate ? (
+          <CreateEditSport onClose={handleClose} />
+        ) : (
+          <div>
+            <div className="flex justify-between">
+              <p className="uppercase font-barlow font-bold text-28">SPORTS</p>
+              <button
+                className="uppercase w-189 h-10 bg-ct4-green-neon font-barlow font-bold text-sm rounded"
+                onClick={handleCreate}
+              >
+                CREATE A NEW SPORT
+              </button>
+            </div>
+            <ListSport
+              actionOpen={handleOpenEdit}
+              actionClose={handleCloseEdit}
+            />
+          </div>
+        )}
       </PageLayout>
     </div>
-  )
+  );
 };
 
 export default SportManagement;
